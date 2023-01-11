c47b982f4b556d7dca6bbf11f82e9c4a016d08d1
jrihanna <rihanna.zekri@gmail.com>
2023-01-12 00:31:28 +1100

    edited the powerpoint

--- Java/javaee/javaeevsdotnet/src/main/java/org/example/BirthApp.java
+++ Java/javaee/javaeevsdotnet/src/main/java/org/example/BirthApp.java
@@ -3,8 +3,21 @@ package org.example;
 import jakarta.ws.rs.ApplicationPath;
 import jakarta.ws.rs.core.Application;
 
+import java.util.Collections;
+import java.util.List;
+
 @ApplicationPath("/")
 public class BirthApp extends Application
 {
+//    public void findMissing(List<Integer> elements, int size) {
+//        Collections.sort(elements);
+//
+//        if(elements.size() != size) {
+//            for(int i = 0; i < elements.size(); i++) {
+//                if(i+1 != elements.get(i))
+//                    System.out.println(i+1);
+//            }
+//        }
+//    }
 
 }
